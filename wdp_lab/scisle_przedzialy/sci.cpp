d04ef29c3695ec79f7c043dd23558eefccfaf3dd
Wojciech Malinowski <wojtekmal@wojciech.malinowski.100@gmail.com>
2025-12-21 12:00:01 +0100

    Updating.

--- wdp_lab/scisle_przedzialy/sci.cpp
+++ wdp_lab/scisle_przedzialy/sci.cpp
@@ -1,4 +1,8 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <array>
+#include <cassert>
 
 template<typename T>
 struct MinimumQueue
@@ -25,11 +29,17 @@ struct MinimumQueue
         {
             elements.pop_back();
         }
+
+        elements.push_back(Element{index, value});
     }
 
-    std::optional<T> minimum_value()
+    bool empty()
     {
-        if (elements.empty()) return std::nullopt;
+        return elements.empty();
+    }
+
+    T minimum_value()
+    {
         return elements.front().value;
     }
 };
@@ -62,41 +72,51 @@ int main()
     int64_t n, U;
     std::cin >> n >> U;
 
-    std::vector<std::array<int64_t, 2>> points(n);
-
-    for (int i = 0; i < n; i++)
-    {
-        std::cin >> points[i][0] >> points[i][1];
-    }
+    std::vector<std::array<int64_t, 2>> points(n + 1);
+    for (int i = 1; i <= n; i++) std::cin >> points[i][0] >> points[i][1];
 
     MinimumQueue<int64_t> smallest_ys;
     MinimumQueue<int64_t> largest_ys;
     MinimumQueue<Segment> best_segments;
-    int64_t r = -1;
+    int64_t r = 0;
 
     auto can_increment_r = [&]()
     {
-        if (r == n + 1) return false;
+        if (r == n) return false;
         int64_t new_y = points[r + 1][1];
-        auto smallest_y = smallest_ys.minimum_value();
-        auto largest_y = largest_ys.minimum_value();
 
-        if (smallest_y.has_value() && abs(*smallest_y - new_y) > U) return false;
-        if (largest_y.has_value() && abs(*largest_y + new_y) > U) return false;
+        if (smallest_ys.empty()) return true;
+        int64_t smallest_y = smallest_ys.minimum_value();
+        int64_t largest_y = largest_ys.minimum_value();
+
+        if (std::abs(smallest_y - new_y) > U) return false;
+        if (std::abs(largest_y + new_y) > U) return false;
         return true;
     };
 
-    for (int l = 0; l < n; l++)
+    for (int64_t l = 1; l <= n; l++)
     {
-        if (!can_increment_r()) continue;
+        bool r_was_incremented = false;
 
         while (can_increment_r())
         {
+            r_was_incremented = true;
             r++;
-            smallest_ys.insert(r, points[l][1]);
-            largest_ys.insert(r, -points[l][1]);
+            smallest_ys.insert(r, points[r][1]);
+            largest_ys.insert(r, -points[r][1]);
+        }
+        
+        if (r_was_incremented)
+        {
+            best_segments.insert(r, Segment{l, r, points[l][0], points[r][0]});
         }
 
-        best_segments.insert(r, Segment{l, r, points[l][0], points[r][0]});
+        assert(!best_segments.empty());
+        auto best_segment = best_segments.minimum_value();
+        std::cout << best_segment.l << " " << best_segment.r << "\n";
+
+        smallest_ys.move_start(l + 1);
+        largest_ys.move_start(l + 1);
+        best_segments.move_start(l + 1);
     }
 }
