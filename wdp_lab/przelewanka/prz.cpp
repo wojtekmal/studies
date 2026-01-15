46e4b92a53febb85ceb585e73bc0aed21e6c300a
Wojciech Malinowski <wojtekmal@wojciech.malinowski.100@gmail.com>
2026-01-15 13:00:01 +0100

    Updating.

---
+++ wdp_lab/przelewanka/prz.cpp
@@ -0,0 +1,99 @@
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+#include <queue>
+using namespace std;
+#define ull unsigned long long
+
+struct Solve
+{
+    struct State
+    {
+        vector<ull> volumes;
+        int cost;
+    };
+
+    int n;
+    vector<ull> cups;
+    vector<ull> targets;
+    unordered_set<ull> hashes;
+    queue<State> states;
+
+    ull hash_vector(const vector<ull>& nums)
+    {
+        ull result = 0;
+        for (ull num : nums) result = result * 1000'000'007 + num;
+        return result;
+    }
+
+    void consider_state(State state)
+    {
+        ull hash = hash_vector(state.volumes);
+
+        if (!hashes.count(hash))
+        {
+            states.push(state);
+            hashes.insert(hash);
+        }
+    }
+
+    void process_state()
+    {
+        vector<ull> volumes = states.front().volumes;
+        int cost = states.front().cost;
+
+        for (int i = 0; i < n; i++)
+        {
+            vector<ull> new_volumes = volumes;
+            new_volumes[i] = 0;
+            consider_state({new_volumes, cost + 1});
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            vector<ull> new_volumes = volumes;
+            new_volumes[i] = cups[i];
+            consider_state({new_volumes, cost + 1});
+        }
+
+        for (int i = 0; i < n; i++) for(int j = 0; j < n; j++)
+        {
+            ull to_pour = min(volumes[i], cups[j] - volumes[j]);
+            vector<ull> new_volumes = volumes;
+            volumes[i] -= to_pour;
+            volumes[j] += to_pour;
+            consider_state({new_volumes, cost + 1});
+        }
+    }
+
+    void run()
+    {
+        cin >> n;
+        cups = vector<ull>(n);
+        targets = vector<ull>(n);
+        for (int i = 0; i < n; i++) cin >> cups[i] >> targets[i];
+
+        State start_state{vector<ull>(n), 0};
+        hashes.insert(hash_vector(start_state.volumes));
+        states.push(start_state);
+
+        while (!states.empty())
+        {
+            if (states.front().volumes == targets)
+            {
+                cout << states.front().cost << "\n";
+                return;
+            }
+
+            process_state();
+        }
+
+        cout << "-1\n";
+    }
+};
+
+int main()
+{
+    Solve solve;
+    solve.run();
+}
