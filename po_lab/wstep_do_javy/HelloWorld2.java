00f55b29448f8941550697b00dc79e3b8b2ddab4
Wojciech Malinowski <wojtekmal@wojciech.malinowski.100@gmail.com>
2026-03-11 13:00:01 +0100

    Updating.

---
+++ po_lab/wstep_do_javy/HelloWorld2.java
@@ -0,0 +1,8 @@
+import hello_world.HelloWorld;
+
+public class HelloWorld2 {
+  public static void main(String[] args) {
+    HelloWorld.main(new String[0]);
+    System.out.println("Hello World 2!");
+  }
+}
---
+++ po_lab/wstep_do_javy/hello_world/HelloWorld.java
@@ -0,0 +1,6 @@
+package hello_world;
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
