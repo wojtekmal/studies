727136fbd0f83a0b8cf8d53b9fa2f702669c8fb9
Wojciech Malinowski <wojtekmal@wojciech.malinowski.100@gmail.com>
2026-03-29 12:00:01 +0200

    Updating.

---
+++ po_lab/szybkosc_javy/baby_step_giant_step.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int64_t descreet_log(int64_t num, int64_t base, int64_t mod)
+{
+    // Find an exponent p such that mod | base^p - num or return -1 if such a
+    // number doesn't exist.
+}
+
+int main()
+{
+    int64_t num, base, mod;
+    cout << discreet_log(num, base, mod) << "\n";
+}
