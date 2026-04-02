b2c47f378156b5327a741a4d59ae8fcf1cfb2812
Wojciech Malinowski <wojtekmal@wojciech.malinowski.100@gmail.com>
2026-04-02 20:00:01 +0200

    Updating.

--- akso_lab/rekurencyjne_stosy/rstack.c
+++ akso_lab/rekurencyjne_stosy/rstack.c
@@ -110,20 +110,92 @@ bool measure_and_flag_scc_dfs(rstack_t* rs, uint64_t* scc_size, uint64_t dfs_id)
         {
             if (element->is_stack == false) continue;
 
-            if (measure_and_flag_scc_dfs(
-                element->contents.stack, scc_size, dfs_id))
-            {
+            if (measure_and_flag_scc_dfs(element->contents.stack, scc_size,
+                dfs_id)) is_in_scc = true;
+        }
 
-            }
+        if (is_in_scc)
+        {
+            *scc_size++;
+            rs->dfs_where_was_part_of_scc = dfs_id;
+        }
+
+        return is_in_scc;
+    }
+}
+
+void count_ready_in_scc_dfs(rstack_t* rs, uint64_t* ready_count,
+    uint64_t dfs_id)
+{
+    if (rs->dfs_where_was_part_of_scc != dfs_id - 1) return;
+
+    if (rs->last_dfs_id != dfs_id)
+    {
+        rs->last_dfs_id = dfs_id;
+        rs->scc_reference_count = 1;
+
+        for (Element* element = rs->top; element != nullptr;
+            element = element->prev_element)
+        {
+            if (element->is_stack == false) continue;
+
+            count_ready_dfs(element->contents.stack, ready_count, dfs_id);
         }
     }
+    else
+    {
+        rs->reference_count++;
+    }
+
+    if (rs->scc_reference_count == rs->reference_count) *ready_count++;
+}
+
+void gather_for_pruning_dfs(rstack_t* rs, uint64_t dfs_id,
+    rstack_t** next_to_prune)
+{
+    if (rs->dfs_where_was_part_of_scc != dfs_id - 2) return;
+    if (rs->last_dfs_id == dfs_id) return;
+
+    rs->last_dfs_id = dfs_id;
+
+    for (Element* element = rs->top; element != nullptr;
+        element = element->prev_element)
+    {
+        if (element->is_stack == false) continue;
+
+        count_ready_dfs(element->contents.stack, dfs_id, next_to_prune);
+    }
+
+    rs->top->contents.stack = *next_to_prune;
+    *next_to_prune = rs;
 }
 
 void rstack_delete(rstack_t *rs)
 {
     if (rs == nullptr) return;
 
-    prune_dfs;
+    uint64_t scc_size = 0;
+    rs->last_dfs_id = next_dfs_id;
+    rs->dfs_where_was_part_of_scc = next_dfs_id;
+    bool _ = measure_and_flag_scc_dfs(rs, &scc_size, next_dfs_id);
+    next_dfs_id++;
+
+    uint64_t ready_count = 0;
+    count_ready_in_scc_dfs(rs, &ready_count, next_dfs_id);
+    next_dfs_id++;
+
+    if (ready_count != scc_size) return;
+
+    if (rs->top == nullptr)
+    {
+        free(rs);
+        return;
+    }
+
+    rstack_t* next_to_prune = nullptr;
+    gather_for_pruning_dfs(rs, next_dfs_id, &next_to_prune);
+
+    prune_stacks_recursively(next_to_prune);
 }
 
 void rstack_pop(rstack_t *rs)
