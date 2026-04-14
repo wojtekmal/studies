849ced2b91f0f2d637e135354346b76dd776e409
Wojciech Malinowski <wojtekmal@wojciech.malinowski.100@gmail.com>
2026-04-14 10:00:02 +0200

    Updating.

--- akso_lab/rekurencyjne_stosy/run_tests.sh
+++ akso_lab/rekurencyjne_stosy/run_tests.sh
@@ -4,7 +4,8 @@ echo
 for test in zero one two three four five memory
 do
     echo $test
-    ./rstack_example $test
+    valgrind --leak-check=yes ./rstack_example $test
+
     diff file_$test.out correct_answers/file_$test.out
     if [ $? -eq 0 ]
     then
@@ -12,6 +13,6 @@ do
     else
         echo WRONG OUTPUT
     fi
-    valgrind --leak-check=yes ./rstack_example zero 2>&1 | tail -n 1
+
     echo
 done
