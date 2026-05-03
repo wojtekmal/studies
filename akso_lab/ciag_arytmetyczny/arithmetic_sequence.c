2fa45d3b7286bde1033e5e6cd3562d9065db7250
Wojciech Malinowski <wojtekmal@wojciech.malinowski.100@gmail.com>
2026-05-03 08:00:01 +0200

    Updating.

--- akso_lab/ciag_arytmetyczny/arithmetic_sequence.c
+++ akso_lab/ciag_arytmetyczny/arithmetic_sequence.c
@@ -12,9 +12,13 @@ int128_t arithmetic_sequence
     (uint64_t const *A0, uint64_t const *A1, uint64_t *Ak, size_t n, int64_t k)
 {
     memcpy(Ak, A0, n);
+    __uint128_t uk = k;
     
     for (int i = 0; i < n; i++)
     {
-        
+        uint64_t diff = A1[i] - A0[i];
+        __uint128_t prod = uk * diff;
+
+        if (*((__uint128_t*) ))
     }
 }
