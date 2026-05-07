eff7412eb59cccb22e6ea50dcb8a78d5946dfd65
Wojciech Malinowski <wojtekmal@wojciech.malinowski.100@gmail.com>
2026-05-07 10:00:01 +0200

    Updating.

--- akso_lab/ciag_arytmetyczny/arithmetic_sequence.asm
+++ akso_lab/ciag_arytmetyczny/arithmetic_sequence.asm
@@ -31,16 +31,36 @@ arithmetic_sequence:
 
 main_loop:
     % Copy current block from A0 into r9.
-    mov r9 [rdi + 8*r15]
+    mov r9, [rdi + 8*r15]
 
     % Copy current block from A1 into r10 and rax.
-    mov r10 [rsi + 8*r15]
-    mov r10 rax
+    mov r10, [rsi + 8*r15]
+    mov r11, r10
 
-    % Store the difference in r11.
-    sub rax r9
+    % Store the difference in r11 and copy to rax.
+    sub r11, r9
+    mov rax, r11
 
     % Multiply (u64) (A1[i] - A0[i]) and (u64) k.
     mul r8
 
-    cmp 
+    % If (i64) A1[i] < (i64) A0[i], subrtract (u64) k from the high bits.
+    cmp r10, r9
+    jge after_sub_k
+    sub rdx, r8
+after_sub_k:
+
+    % We won't need A1[i] anymore, so r10 is free.
+
+    % If (i64) k < 0, subtract the difference from the high bits of the product.
+    cmp r8, 0
+    jge after_sub_diff
+    sub rdx, r11
+after_sub_diff:
+
+    % We won't need the difference anymore, so r11 is free.
+    % Right now rdx:rax holds a signed 128 bit representation of
+    % k * (A1[i] - A0[i]), where k is signed and A1[i], A0[i] are unsigned.
+
+    % Copy the lower bits of the product into r10.
+    mov r10, rax
