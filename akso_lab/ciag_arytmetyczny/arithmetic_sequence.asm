69d35937059ab4c2dccb53ae42b2f6672ae6169c
Wojciech Malinowski <wojtekmal@wojciech.malinowski.100@gmail.com>
2026-05-07 09:00:01 +0200

    Updating.

--- akso_lab/ciag_arytmetyczny/arithmetic_sequence.asm
+++ akso_lab/ciag_arytmetyczny/arithmetic_sequence.asm
@@ -8,8 +8,7 @@ arithmetic_sequence:
     push r12
     push rbx
 
-    % rbx will hold k
-    mov rbx, r8
+    % r8 will hold k. It already happens to be here.
 
     % r14 will hold n
     mov r14, rcx
@@ -17,10 +16,31 @@ arithmetic_sequence:
     % r12 will hold pointer to Ak
     mov r12, rdx
 
-    % r15 will hold pointer to A1
-    mov r15, rsi
+    % rsi will hold pointer to A1. It already happens to be here.
 
-    % r13 will hold pointer to A0
-    mov r13, rdi
+    % rdi will hold pointer to A0. It already happens to be here.
 
-    
+    % rbx will hold remainder.
+    xor rbx, rbx
+
+    % r13 will hold the high bits from the previous iteration.
+    xor r13, r13
+
+    % r15 will hold the current position (goes from 0 to n - 1).
+    xor r15, r15
+
+main_loop:
+    % Copy current block from A0 into r9.
+    mov r9 [rdi + 8*r15]
+
+    % Copy current block from A1 into r10 and rax.
+    mov r10 [rsi + 8*r15]
+    mov r10 rax
+
+    % Store the difference in r11.
+    sub rax r9
+
+    % Multiply (u64) (A1[i] - A0[i]) and (u64) k.
+    mul r8
+
+    cmp 
