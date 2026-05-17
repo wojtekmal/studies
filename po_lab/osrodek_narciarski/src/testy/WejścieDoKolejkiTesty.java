d5a0766d472b04cdc92dd84c0a3945c284a9a131
Wojciech Malinowski <wojtekmal@wojciech.malinowski.100@gmail.com>
2026-05-17 13:00:01 +0200

    Updating.

--- po_lab/osrodek_narciarski/src/testy/WejścieDoKolejkiTesty.java
+++ po_lab/osrodek_narciarski/src/testy/WejścieDoKolejkiTesty.java
@@ -1,5 +1,8 @@
 package testy;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -7,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import połączenie.Wyciąg;
 import sportowiec.Sportowiec;
+import uk.org.webcompere.systemstubs.SystemStubs;
 import zdarzenie.Godzina;
 import zdarzenie.WejścieDoKolejki;
 
@@ -21,10 +25,21 @@ public class WejścieDoKolejkiTesty
     private Wyciąg wyciąg;
 
     @Test
-    public void testujZaraportuj()
+    public void testujZaraportuj() throws Exception
     {
-        WejścieDoKolejki wejścieDoKolejki = new WejścieDoKolejki(godzina, sportowiec, wyciąg);
-        wejścieDoKolejki.zaraportuj();
+        when(godzina.toString()).thenReturn("10:10:10");
+        when(sportowiec.id()).thenReturn(3);
+        when(wyciąg.id()).thenReturn(5);
+        when(sportowiec.czyŚledzić()).thenReturn(true);
 
+        WejścieDoKolejki wejścieDoKolejki = new WejścieDoKolejki(godzina, sportowiec, wyciąg);
+
+        String raport = SystemStubs.tapSystemOut(() ->
+        {
+            wejścieDoKolejki.zaraportuj();
+        });
+        String oczekiwanyRaport = "10:10:10: Sportowiec 3 ustawił się w kolejce pod wyciągiem 5.\n";
+
+        assertEquals(oczekiwanyRaport, raport);
     }
 }
