88da054bfc885fd216f2d4992d2fa206fb999e35
Wojciech Malinowski <wojtekmal@wojciech.malinowski.100@gmail.com>
2026-05-20 14:00:02 +0200

    Updating.

--- akso_lab/discrete_fractal/discrete_fractal.c
+++ akso_lab/discrete_fractal/discrete_fractal.c
@@ -8,9 +8,18 @@ int convert_to_int(char* n_string, uint32_t* n)
     
     for (int i = 0; n_string[i] != 0; i++)
     {
-        if (n > UINT_MAX / 10u)
+        uint32_t digit = n_string[i] - '0';
+        if (digit > 9) return 1;
+
+        if (*n > (UINT_MAX - digit) / 10u) return 1;
+
+        *n = *n * 10 + digit;
     }
+
+    return 0;
 }
+
+char* exchange_strings[128];
 
 int main(int argument_count, char** arguments)
 {
@@ -19,4 +28,20 @@ int main(int argument_count, char** arguments)
     char* n_string = arguments[0];
     uint32_t n;
     int conversion_result = convert_to_int(n_string, &n);
+    if (conversion_result != 0) return 1;
+
+    char* start_string = nullptr;
+    getline(&start_string, nullptr, 0);
+
+    char* exchange_string;
+
+    while (getline(&exchange_string, nullptr, 0) != -1)
+    {
+        exchange_strings[exchange_string[0]] = exchange_string + 1;
+    }
+
+    for (uint64_t i = 0; start_string[i] != 0; i++)
+    {
+        dfs(start_string[i]);
+    }
 }
